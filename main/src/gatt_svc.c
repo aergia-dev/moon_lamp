2cb9d7850bcf43c52a027e014e486a725b2c5322
aergia <aergia.dev@gmail.com>
2025-01-11 13:37:22 +0900

    add ble ota default code
    * not sure *

--- main/src/gatt_svc.c
+++ main/src/gatt_svc.c
@@ -8,6 +8,7 @@
 #include "common.h"
 #include "embedded_led.h"
 #include "ble_protocol.h"
+#include "esp_ota_ops.h"
 
 /* Private function declarations */
 static int sleep_light_chr_access(uint16_t conn_handle, uint16_t attr_handle,
@@ -21,7 +22,7 @@ static handler_rsp_t rsp = {
     .len = 0,
 };
 
-/* Private variables */
+// light service
 static const ble_uuid128_t sleep_light_svc_uuid =
     BLE_UUID128_INIT(0xA1, 0xB2, 0xC3, 0xD4,
                      0x00, 0x00, 0x10, 0x00,
@@ -39,6 +40,74 @@ static const ble_uuid128_t sleep_light_chr_uuid =
 static uint16_t sleep_light_chr_conn_handle = 0;
 static bool sleep_light_chr_conn_handle_inited = false;
 static bool sleep_light_ind_status = false;
+// OTA service
+static const ble_uuid128_t gatt_svr_svc_ota_uuid =
+    BLE_UUID128_INIT(0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15,
+                     0xDE, 0xEF, 0x12, 0x12, 0x30, 0x15, 0x00, 0x00);
+
+static const ble_uuid128_t gatt_svr_chr_ota_control_uuid =
+    BLE_UUID128_INIT(0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15,
+                     0xDE, 0xEF, 0x12, 0x12, 0x30, 0x15, 0x01, 0x00);
+
+static const ble_uuid128_t gatt_svr_chr_ota_data_uuid =
+    BLE_UUID128_INIT(0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15,
+                     0xDE, 0xEF, 0x12, 0x12, 0x30, 0x15, 0x02, 0x00);
+
+static esp_ota_handle_t ota_handle = 0;
+static const esp_partition_t *update_partition = NULL;
+static bool ota_in_progress = false;
+
+enum ota_control
+{
+    OTA_START = 0x01,
+    OTA_DATA = 0x02,
+    OTA_END = 0x03,
+};
+
+static int gatt_svr_chr_ota_control_cb(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    if (ctxt->op == BLE_GATT_ACCESS_OP_WRITE_CHR)
+    {
+        uint8_t command;
+        memcpy(&command, ctxt->om->om_data, sizeof(command));
+
+        switch (command)
+        {
+        case OTA_START:
+            if (!ota_in_progress)
+            {
+                update_partition = esp_ota_get_next_update_partition(NULL);
+                ESP_ERROR_CHECK(esp_ota_begin(update_partition, OTA_SIZE_UNKNOWN, &ota_handle));
+                ota_in_progress = true;
+                ESP_LOGI(TAG, "OTA start");
+            }
+            break;
+        case OTA_END:
+            if (ota_in_progress)
+            {
+                ESP_ERROR_CHECK(esp_ota_end(ota_handle));
+                ESP_ERROR_CHECK(esp_ota_set_boot_partition(update_partition));
+                ota_in_progress = false;
+                ESP_LOGI(TAG, "OTA end");
+                esp_restart();
+            }
+        }
+    }
+    return 0;
+}
+
+static int gatt_svr_chr_ota_data_cb(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    if (ctxt->op == BLE_GATT_ACCESS_OP_WRITE_CHR)
+    {
+        if (ota_in_progress)
+        {
+            ESP_ERROR_CHECK(esp_ota_write(ota_handle, ctxt->om->om_data, OS_MBUF_PKTLEN(ctxt->om)));
+            ESP_LOGI(TAG, "OTA: Recevied %d bytes", OS_MBUF_PKTLEN(ctxt->om));
+        }
+    }
+    return 0;
+}
 
 /* GATT services table */
 static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
@@ -55,6 +124,25 @@ static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
              {
                  0, /* No more characteristics in this service. */
              }}},
+    {
+        .type = BLE_GATT_SVC_TYPE_PRIMARY,
+        .uuid = &gatt_svr_svc_ota_uuid.u,
+        .characteristics = (struct ble_gatt_chr_def[]){
+            {
+                .uuid = &gatt_svr_chr_ota_control_uuid.u,
+                .access_cb = gatt_svr_chr_ota_control_cb,
+                .flags = BLE_GATT_CHR_F_WRITE,
+            },
+            {
+                .uuid = &gatt_svr_chr_ota_data_uuid.u,
+                .access_cb = gatt_svr_chr_ota_data_cb,
+                .flags = BLE_GATT_CHR_F_WRITE,
+            },
+            {
+                0, /* No more characteristics in this service. */
+            },
+        },
+    },
     {
         0, /* No more services. */
     },
